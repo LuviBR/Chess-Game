dc2d776dc990efc3121f14fe4603e4d51a5ff906
Lucas Viana <lucas_viana99@outlook.com>
2020-05-20 10:26:25 -0300

    Implementando as retricoes de movimento

--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -17,8 +17,24 @@ namespace xadrez_console.tabuleiro
 			this.qteMovimentos = 0;
 		}
 
-		public void incrementarQteMOvimentos() {
+		public void incrementarQteMovimentos() {
 			qteMovimentos++;
+		}
+		
+		public bool existeMovimentosPossiveis() {
+			bool[,] mat = movimentosPossiveis();
+			for (int i=0; i<tab.linhas; i++) {
+				for (int j= 0; j<tab.colunas; j++) {
+					if (mat[i, j]) {
+						return true;
+					}
+				}
+			}
+			return false;
+		}
+
+		public bool podeMoverPara(Posicao pos) {
+			return movimentosPossiveis()[pos.linha, pos.coluna];
 		}
 
 		public abstract bool[,] movimentosPossiveis();
--- xadrez-console/xadrez/PartidaDeXadrez.cs
+++ xadrez-console/xadrez/PartidaDeXadrez.cs
@@ -7,8 +7,8 @@ namespace xadrez
 	class PartidaDeXadrez
 	{
 		public Tabuleiro tab { get; private set; }
-		private int turno;
-		private Cor jogadorAtual;
+		private int turno { get;  set; }
+		private Cor jogadorAtual { get; set; }
 		public bool terminada { get; private set; }
 
 		public PartidaDeXadrez()
@@ -22,9 +22,45 @@ namespace xadrez
 		public void executaMovimento(Posicao origem, Posicao destino)
 		{
 			Peca p = tab.retirarPeca(origem);
-			p.incrementarQteMOvimentos();
+			p.incrementarQteMovimentos();
 			Peca pecaCaprturada = tab.retirarPeca(destino);
 			tab.colocarPeca(p, destino);
+		}
+
+		public void realizaJogada(Posicao origem, Posicao destino)
+		{
+			executaMovimento(origem, destino);
+			turno++;
+			mudaJogador();
+		} 
+
+		public void validarPosicaoDeOrigem(Posicao pos) {
+			if(tab.peca(pos) == null) {
+				throw new TabuleiroException("Não existe peça na posição de origem escolhida!");
+			}
+			if(jogadorAtual != tab.peca(pos).cor) {
+				throw new TabuleiroException("A peça de origem escolhida não é sua!");
+			}
+			if(!tab.peca(pos).existeMovimentosPossiveis()){
+				throw new TabuleiroException("Não há movimentos possiveis para a peça de origem escolhida! ");
+			}
+		}
+
+		public void validarPosicaoDeDestino(Posicao origem, Posicao destino) 
+		{
+			if(!tab.peca(origem).podeMoverPara(destino)) {
+				throw new TabuleiroException("Posição de destino inválida! ");
+			}
+		}
+
+		private void mudaJogador()
+		{
+			if(jogadorAtual == Cor.Branca) {
+				jogadorAtual = Cor.Preta;
+			}
+			else {
+				jogadorAtual = Cor.Branca;
+			}
 		}
 
 		private void colocarPecas() {
